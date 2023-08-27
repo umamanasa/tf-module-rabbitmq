32363c545ae2b30181d1ef81fba739595b257838
umamanasa <umamanasa49@gmail.com>
2023-08-27 16:20:35 +0530

    creating rabbitmq instance,sg's

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 yum install ansible -y &>>/opt/userdata.log
-ansible-playbook -i localhost, -U https://github.com/umamanasa/roboshop-ansible.git main.yml -e component=rabbiitmq &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/umamanasa/roboshop-ansible.git main.yml -e component=rabbiitmq &>>/opt/userdata.log
